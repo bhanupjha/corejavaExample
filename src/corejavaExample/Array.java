8041204b34551e676e21fef83fb46084f9efa51c
bhanupjha <mailtobhanup@gmail.com>
2022-10-25 21:33:34 +0530

    new program added.

---
+++ src/corejavaExample/Array.java
@@ -0,0 +1,33 @@
+package corejavaExample;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int[] anArray;
+	anArray = new int[10];
+	
+	anArray[0] = 1;
+	anArray[1] = 2;
+	anArray[2] = 3;
+	anArray[3] = 4;
+	anArray[4] = 5;
+	anArray[5] = 6;
+	anArray[6] = 7;
+	anArray[7] = 8;
+	anArray[8] = 9;
+	anArray[9] = 10;
+	
+	System.out.println("Element at index at 0: " + anArray[0]);
+	System.out.println("Element at index at 1: " + anArray[1]);
+	System.out.println("Element at index at 2: " + anArray[2]);
+	System.out.println("Element at index at 3: " + anArray[3]);
+	System.out.println("Element at index at 4: " + anArray[4]);
+	System.out.println("Element at index at 5: " + anArray[5]);
+	System.out.println("Element at index at 6: " + anArray[6]);
+	System.out.println("Element at index at 7: " + anArray[7]);
+	System.out.println("Element at index at 8: " + anArray[8]);
+	System.out.println("Element at index at 9: " + anArray[9]);
+	
+	}
+
+}
--- src/corejavaExample/helloworld.java
+++ src/corejavaExample/helloworld.java
@@ -1,9 +1,9 @@
 package corejavaExample;
 
-public class helloworld {
+public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD!");
 
 	}
 
