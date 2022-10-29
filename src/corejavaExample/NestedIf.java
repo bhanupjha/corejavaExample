86d56e6d2350675ff2a3346aa1784e1c268af87a
bhanupjha <mailtobhanup@gmail.com>
2022-10-29 09:55:21 +0530

    new program added.

---
+++ src/corejavaExample/NestedIf.java
@@ -0,0 +1,21 @@
+package corejavaExample;
+
+public class NestedIf {
+
+	public static void main(String[] args) {
+		
+		int i = 50;
+		
+		if(i == 50) {
+			System.out.println("i is 50");
+		if (i <75) {
+			System.out.println("i is smaller than 50");
+		if (i <55) {
+            System.out.println("i is also smaller than 50");
+	}
+		
+		}
+		}
+	}
+
+}
---
+++ src/corejavaExample/TwoDimensionalArray.java
@@ -0,0 +1,12 @@
+package corejavaExample;
+
+public class TwoDimensionalArray {
+
+	public static void main(String[] args) {
+	
+		int arr[][] = {{2,7,9}, {3,6,1}, {7,4,2}};
+		
+		System.out.println(arr[2][1]);
+	}
+	
+}
