516c5e6fb7d0bf83fc2463fc5807b6e6d5998573
bhanupjha <mailtobhanup@gmail.com>
2022-12-28 19:24:36 +0530

    New program

---
+++ src/corejavaExample/BitwiseOperaor.java
@@ -0,0 +1,23 @@
+package corejavaExample;
+
+public class BitwiseOperaor {
+
+	public static void main(String[] args) {
+	
+		//initial value
+		int a = 5;
+		int b = 7;
+		
+		//bitwise and
+		//0101& 0111 = 0101 =5 
+		System.out.println("a & b = " + (a & b));
+		
+		// bitwise or
+		// 0101 | 0111 = 0111 = 7
+		System.out.println("a | b = " + (a | b));
+		
+		
+
+	}
+
+}
