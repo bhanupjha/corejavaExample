170293c40f5b619a3243d30add44f832ef4d6df5
bhanupjha <mailtobhanup@gmail.com>
2022-12-31 13:40:20 +0530

    New program added

---
+++ src/corejavaExample/Datatype/PrimitiveDataTypes.java
@@ -0,0 +1,33 @@
+package corejavaExample.Datatype;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		
+		 byte a = 26;
+		 
+		 short b = 2388;
+		 
+		 int c = 233656689;
+		 
+		 long d = 2333;
+		 
+		 float e = 23.456f;
+		 
+		 double f = 234.6789d;
+		 
+		 boolean g =  true;
+		 
+		 char  h  = 'a';
+		 
+		 System.out.println("Byte: " + a);
+		 System.out.println("Short: "+ b);
+		 System.out.println("Int: "+c);
+		 System.out.println("Long: "+d);
+		 System.out.println("float: "+e);
+		 System.out.println("double: "+f);
+		 System.out.println("boolean: "+g);
+		 System.out.println("char: "+h);
+   }
+
+}
