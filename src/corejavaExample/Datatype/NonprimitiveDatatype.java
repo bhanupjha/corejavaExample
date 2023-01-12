47020d9ccafd9a24195fc9423b20317e771d52a9
bhanupjha <mailtobhanup@gmail.com>
2023-01-13 00:14:11 +0530

    New program added.

--- src/corejavaExample/Datatype/NonprimitiveDatatype.java
+++ src/corejavaExample/Datatype/NonprimitiveDatatype.java
@@ -1,24 +1,34 @@
 package corejavaExample.Datatype;
 
-
 public class NonprimitiveDatatype{
-	public static void main(String[] args) {
-		  String str1 = "ABC";
-		  System.out.println("string: " +str1);
-		  
-		  String str2 = new String("ABC");
-		  System.out.println("Another string: " +str2);
-		  
-		  
-		  
-		  
-		int arr[];  
-		arr = new int[2];
-		arr[0] = 1;
-		arr[1] = 2;
+	
+	public static void  main(String[] args) {
 		
-		System.out.println("Element at index at 0: " +arr[0]);
-		System.out.println("Element at index at 1: " +arr[1]);
+		String str1 = "ABC";
+		System.out.println("String: " +str1);
 		
+//Another way of creating it	
+		String str2 = new String("ABC");
+		System.out.println("Another string: " +str2);
+		
+		
+		
+//First method
+	int arr[];
+       arr = new int[2];
+       arr[0] = 1;
+       arr[1] = 2;
+    // arr[2] = 8;  (Array has the size of only of two element. it doesn't have a capacity to store third element)
+       
+	   System.out.println("Array: " +arr); // printing memory footprint           
+       System.out.println("Element in index 0: " +arr[0]);
+	   System.out.println("Element in index 1: " +arr[1]);
+	 //System.out.println("Element in index 2: " +arr[2]);
+	   
+//Second Method
+	  int array[] = {7,8};
+	  	   System.out.println("First element : " +array[0]);
+	       System.out.println("Second element: " +array[1]);
+	   
 	}
 }
---
+++ src/corejavaExample/Datatypeconversion/ImplicitDataTypeConversion.java
@@ -0,0 +1,27 @@
+package corejavaExample.Datatypeconversion;
+
+public class ImplicitDataTypeConversion {
+	 public static void main(String[] args) {
+		 
+		byte a = 10;
+		System.out.println("Byte Representation: " +a);
+		
+		short b = a;
+		System.out.println("short Representation: " +b);
+		
+		int c = b;
+		System.out.println("Int representation: " +c);
+		 
+		long d= c;
+		System.out.println("Long Representation: " +d);
+		
+		float e = d;
+		System.out.println("Float Representation: " +e);
+		
+		double f = e;
+		System.out.println("Double Representation: " +f);
+		 
+	 }
+	
+
+}
